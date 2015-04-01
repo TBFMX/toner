66122374dba251ec4fb2d4e5b9db27cdb7bfff93
Abel Chavez <abchavz@yahoo.com.mx>
2015-04-01 00:24:37 -0600

    adjusting database fields and automating csv upload

--- app/models/cartucho.rb
+++ app/models/cartucho.rb
@@ -1,121 +1,116 @@
 class Cartucho < ActiveRecord::Base
-	has_many :line_items
-	before_destroy :ensure_not_referenced_by_any_line_item
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
-	#nota: pude haber usado push pero es para que fuera mas facil de entender y fuera simetrico
-	def self.query(cartucho)
-	    @aux= " 1 = 1 "
-	    @aux2= Array.new
-	    @cont=1
-	    if cartucho
-		    if !cartucho[:brand].blank?
-		    	@aux= @aux + 'and brand = ? '
-		    	@aux2[@cont]=  cartucho[:brand]
-		    	@cont=@cont+1
-		    end
-		    if !cartucho[:impresoras].blank?
-		    	@aux= @aux + 'and impresoras = ? '
-		    	@aux2[@cont]=  cartucho[:impresoras]
-		    	@cont=@cont+1
-		    end
-		end
-	    if @aux
-	    #puts @cont	
-		      @aux2[0]=@aux
-	      where @aux2 
-	    else
-	      scoped
-	    end
+  #nota: pude haber usado push pero es para que fuera mas facil de entender y fuera simetrico
+  def self.query(cartucho)
+    @aux= " 1 = 1 "
+    @aux2= Array.new
+    @cont=1
+    if cartucho
+      if !cartucho[:brand].blank?
+        @aux= @aux + 'and brand = ? '
+        @aux2[@cont]=  cartucho[:brand]
+        @cont=@cont+1
+      end
+      if !cartucho[:impresoras].blank?
+        @aux= @aux + 'and impresoras = ? '
+        @aux2[@cont]=  cartucho[:impresoras]
+        @cont=@cont+1
+      end
+    end
+    if @aux
+      #puts @cont	
+      @aux2[0]=@aux
+      where @aux2 
+    else
+      scoped
+    end
 
-	end	
+  end	
 
-	def self.search(modelo_c,marca,modelo_i)
-		
-		listo = false
+  def self.search(modelo_c,marca,modelo_i)
 
-		
-		
-		
-	@aux= " 1 = 1 "
-	    @aux2= Array.new
-	    @cont=1
-	    
-		    if !modelo_c.blank?
-		    	modelo_sc = '%' + modelo_c + '%'
-		    	modelo_sc2 = '%' + modelo_c.upcase + '%'
-		    	@aux= @aux + 'and (clave LIKE ? or clave LIKE ?)'
-		    	@aux2[@cont]=  modelo_sc
-		    	@cont=@cont+1
-		    	@aux2[@cont]=  modelo_sc2
-		    	@cont=@cont+1
-		    	
-		    end
-		    if !marca.blank?
-		    	@aux= @aux + 'and brand = ? '
-		    	@aux2[@cont]=  marca
-		    	@cont=@cont+1
-		    end
-		    if !modelo_i.blank?
-		    	modelo_si = '%' + modelo_i + '%'
-		    	@aux= @aux + 'and impresoras LIKE ? '
-		    	@aux2[@cont]= modelo_si
-		    	@cont=@cont+1
-		    end
+    listo = false
 
-		    
+    @aux= " 1 = 1 "
+    @aux2= Array.new
+    @cont=1
 
-		    
-	    if @aux
-	    #puts @cont	
-		      @aux2[0]=@aux
-		      
-	      where @aux2 
-	    else
-	      scoped
-	    end
+    if !modelo_c.blank?
+      modelo_sc = '%' + modelo_c + '%'
+      modelo_sc2 = '%' + modelo_c.upcase + '%'
+      @aux= @aux + 'and (clave LIKE ? or clave LIKE ?)'
+      @aux2[@cont]=  modelo_sc
+      @cont=@cont+1
+      @aux2[@cont]=  modelo_sc2
+      @cont=@cont+1
 
-	end
-	def self.search_imp(modelo,marca)
-		listo = false
+    end
+    if !marca.blank?
+      @aux= @aux + 'and brand = ? '
+      @aux2[@cont]=  marca
+      @cont=@cont+1
+    end
+    if !modelo_i.blank?
+      modelo_si = '%' + modelo_i + '%'
+      @aux= @aux + 'and impresoras LIKE ? '
+      @aux2[@cont]= modelo_si
+      @cont=@cont+1
+    end
 
-		aux = '%' + modelo + '%'
-		aux2 = '%' + marca + '%'
-		unless modelo.blank? || marca.blank?
-			
-			#aux = search 
-			puts "entron ambos"
-			listo = true
-			return where('model LIKE ? or brand LIKE ? ', aux,aux2)
-			#.order('package DESC,rank ASC, priority ASC')
-			
-		else
-			unless modelo.blank?
-				listo = true
-				puts "entro modelo"
-				return where('impresoras LIKE ? ', aux)
-				
-		    end
-		    unless marca.blank?
-		    	listo = true
-				puts "entro marca"
-				puts aux2
-		    	return where('brand LIKE ? ', aux2)
-				
-		    end
-		end
-		if listo == false
-			all
-		end
+    if @aux
+      #puts @cont	
+      @aux2[0]=@aux
 
-	end
+      where @aux2 
+    else
+      scoped
+    end
 
-	private
-		def ensure_not_referenced_by_any_line_item
-			if line_items.empty?
-				return true
-			else
-				errors.add(:base, 'Line Items Present')
-				return false
-			end
-		end
+  end
+
+  def self.search_imp(modelo,marca)
+    listo = false
+
+    aux = '%' + modelo + '%'
+    aux2 = '%' + marca + '%'
+    unless modelo.blank? || marca.blank?
+
+      #aux = search 
+      puts "entron ambos"
+      listo = true
+      return where('model LIKE ? or brand LIKE ? ', aux,aux2)
+      #.order('package DESC,rank ASC, priority ASC')
+
+    else
+      unless modelo.blank?
+        listo = true
+        puts "entro modelo"
+        return where('impresoras LIKE ? ', aux)
+
+      end
+      unless marca.blank?
+        listo = true
+        puts "entro marca"
+        puts aux2
+        return where('brand LIKE ? ', aux2)
+
+      end
+    end
+    if listo == false
+      all
+    end
+
+  end
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items Present')
+      return false
+    end
+  end
 end
